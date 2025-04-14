0bd590dd615d2ad2edceeba27ddedd6fec2b8fcd
sangyong34 <bagsangyong34@gmail.com>
2025-04-14 17:24:10 +0900

    first-commit

---
+++ src/main/java/bangdori/api_mybatis/ApiMybatisApplication.java
@@ -0,0 +1,13 @@
+package bangdori.api_mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMybatisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bangdori/api_mybatis/ApiMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package bangdori.api_mybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMybatisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
