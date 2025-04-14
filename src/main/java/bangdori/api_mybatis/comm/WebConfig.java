cd1924a5a5d2f040ba27adc65d1c5c6956acbe73
sangyong34 <bagsangyong34@gmail.com>
2025-04-14 18:24:17 +0900

    feat/백엔드 WebConfig 파일 설정 완료

---
+++ src/main/java/bangdori/api_mybatis/comm/WebConfig.java
@@ -0,0 +1,27 @@
+package bangdori.api_mybatis.comm;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/**")
+//                .addResourceLocations("classpath:/static/")
+//                .setCacheControl(CacheControl.noCache().cachePrivate().mustRevalidate());
+//    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+}
