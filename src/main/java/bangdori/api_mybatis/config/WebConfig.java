718d6bb44b22dff789266750d1e0019a8a91f73c
sangyong34 <bagsangyong34@gmail.com>
2025-04-15 17:06:07 +0900

    fix:WebConfig 파일 폴더 변경

---
+++ src/main/java/bangdori/api_mybatis/config/WebConfig.java
@@ -0,0 +1,25 @@
+package bangdori.api_mybatis.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/**")
+//                .addResourceLocations("classpath:/static/")
+//                .setCacheControl(CacheControl.noCache().cachePrivate().mustRevalidate());
+//    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+}
