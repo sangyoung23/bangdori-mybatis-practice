16838b93e3d3a3b15729811731e6ef71c72898b8
sangyong34 <bagsangyong34@gmail.com>
2025-04-22 16:02:50 +0900

    feat:최신일자 update Mapper interface 개발

--- src/main/java/bangdori/api_mybatis/domain/product/mapper/ProductMapper.java
+++ src/main/java/bangdori/api_mybatis/domain/product/mapper/ProductMapper.java
@@ -2,11 +2,14 @@ package bangdori.api_mybatis.domain.product.mapper;
 
 import bangdori.api_mybatis.domain.product.vo.ProductVO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 @Mapper
 public interface ProductMapper {
 
-    List<ProductVO> selectProducts();
+    List<ProductVO> selectProducts(@Param("corpNo") Long corpNo);
+
+    void updateNewDtmAndUser(@Param("prodNo") Long prodNo, @Param("userNo") Long userNo);
 }
