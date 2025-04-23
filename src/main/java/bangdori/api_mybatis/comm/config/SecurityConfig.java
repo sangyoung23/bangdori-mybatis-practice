302d50d906857fb1633d7b3090f8760cb07f2e78
sangyong34 <bagsangyong34@gmail.com>
2025-04-23 17:55:05 +0900

    feature:PasswordEncoder 암호화 방식 빈 등록

--- src/main/java/bangdori/api_mybatis/comm/config/SecurityConfig.java
+++ src/main/java/bangdori/api_mybatis/comm/config/SecurityConfig.java
@@ -10,6 +10,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -19,6 +21,13 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
     private final JwtFilter jwtFilter;
+
+
+    // 스프링 시큐리티는 PasswordEncoder를 기본 설정만 해줄 뿐 암호화 방식을 명시적으로 설정하여 빈 등록을 해야함
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
