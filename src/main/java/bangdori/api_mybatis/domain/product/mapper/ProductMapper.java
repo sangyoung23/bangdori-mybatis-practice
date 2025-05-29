a0767a92280482a080bf5de7d7c5c60e4056f2c5
sangyong34 <bagsangyong34@gmail.com>
2025-05-29 17:06:54 +0900

    feature:매물 등록 관련 mapper interface 생성

--- src/main/java/bangdori/api_mybatis/domain/product/mapper/ProductMapper.java
+++ src/main/java/bangdori/api_mybatis/domain/product/mapper/ProductMapper.java
@@ -1,8 +1,12 @@
 package bangdori.api_mybatis.domain.product.mapper;
 
+import bangdori.api_mybatis.domain.product.dto.ProductRequestDto;
+import bangdori.api_mybatis.domain.product.vo.ProductImageVO;
+import bangdori.api_mybatis.domain.product.vo.ProductRemarkVO;
 import bangdori.api_mybatis.domain.product.vo.ProductVO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -14,4 +18,10 @@ public interface ProductMapper {
     void updateNewDtmAndUser(@Param("prodNo") Long prodNo, @Param("userNo") Long userNo);
 
     void deleteProduct(@Param("prodNo") Long prodNo);
+
+    void insertProduct(ProductVO product);
+
+    void insertRemarks(@Param("remarkList") List<ProductRemarkVO> remarkList);
+
+    void insertImages(@Param("imageList") List<ProductImageVO> imageList);
 }
